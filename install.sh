c9250478eb6054eb601f6b8b15d1a00552254532
GameProarmy <63298525+GameProarmy@users.noreply.github.com>
2022-02-02 14:34:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ pkg install wget -y
 pkg install ffmpeg libwebp -y
 pkg install nodejs -y
 pkg install tesseract -y
-wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/gameproarmy/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 npm audit fix
 
